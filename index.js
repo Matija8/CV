7834552e9a4463baddd8541f47fd2385a8db6e4f
matija8 <matijanme@gmail.com>
2022-04-25 13:14:39 +0200

    feat: Add Team Sava link

--- index.js
+++ index.js
@@ -15,18 +15,13 @@ function setCardQueryText(card, query, text) {
   cardQueryGet(card, query).textContent = text;
 }
 
-// function setCardQueryTextFst(card, query, text) {
-//   card.querySelector(query)[0].textContent = text;
-// }
-
 console.log(workExpTemplate);
 function makeWorkExpItem({
-  companyTitle,
+  companies,
   title,
   bodyRight,
   duration,
   location,
-  companyHref,
   companyLogo,
   companyLogoStyle,
 }) {
@@ -37,10 +32,11 @@ function makeWorkExpItem({
     setCardQueryText(card, prefixQuery(query), text);
   qtext('worker-title', title);
   qtext('duration', duration);
-  qtext('location', location);
-  qtext('company-title', companyTitle);
+  qtext('location', location ?? locations.belgrade);
   qtext('body-right', bodyRight);
-  qget('company-title').href = companyHref;
+  qget('company-title').innerHTML = companies
+    .map(({ title, href }) => `<a href="${href}">${title}</a>`)
+    .join(' / ');
   const logo = qget('company-logo');
   logo.style.backgroundImage = `url('assets/companies/${companyLogo}')`;
   if (companyLogoStyle) {
@@ -50,45 +46,44 @@ function makeWorkExpItem({
 }
 
 makeWorkExpItem({
-  companyTitle: 'Unleash',
+  companies: [
+    { title: 'Unleash', href: 'https://www.unleash.so/' },
+    { title: 'Team Sava', href: 'https://team-sava.com/' },
+  ],
   title: 'Full Stack Developer',
   bodyRight: `
-Helping build the Unleash app 🚀 (https://www.unleash.so/features).
-Working on Node.js microservices that integrate 3rd party APIs (https://www.unleash.so/integrations).`,
+  Helping build the Unleash app 🚀 (https://www.unleash.so/features).
+  Working on Node.js microservices that integrate 3rd party APIs (https://www.unleash.so/integrations).`,
   duration: 'Feb 2022 - May 2022',
-  location: locations.belgrade,
-  companyHref: 'https://www.unleash.so/',
   companyLogo: 'unleash.jpg',
   companyLogoStyle: { height: '33px' },
 });
 
 makeWorkExpItem({
-  companyTitle: 'InsideMaps Inc',
+  companies: [{ title: 'InsideMaps Inc', href: 'https://www.insidemaps.com/' }],
   title: 'Junior Software Engineer',
   bodyRight: `
-Full stack web development, working on a React, AngularJS
-(yes, the old one), Node.js, AWS, Typescript and MongoDB based
-infrastructure. Developing and maintaining features for
-internal tools. Responsible for maintaining logging and
-profiling logic inside these tools. Working in an agile team
-with 2 week sprints.`,
+  Full stack web development, working on a React, AngularJS
+  (yes, the old one), Node.js, AWS, Typescript and MongoDB based
+  infrastructure. Developing and maintaining features for
+  internal tools. Responsible for maintaining logging and
+  profiling logic inside these tools. Working in an agile team
+  with 2 week sprints.`,
   duration: 'Apr 2021 - Feb 2022',
-  location: locations.belgrade,
-  companyHref: 'https://www.insidemaps.com/',
   companyLogo: 'inside-maps.jpg',
 });
 
 makeWorkExpItem({
-  companyTitle: 'Microsoft',
+  companies: [
+    { title: 'Microsoft', href: 'https://www.microsoft.com/en-rs/mdcs' },
+  ],
   title: 'Software Engineer Intern',
   bodyRight: `
-With the help of a mentor, added new functionality to C++/C#
-based microservices used in production by Microsoft PowerPoint.
-Communicated and adapted design decisions with colleagues from
-Sunnyvale, CA.`,
+  With the help of a mentor, added new functionality to C++/C#
+  based microservices used in production by Microsoft PowerPoint.
+  Communicated and adapted design decisions with colleagues from
+  Sunnyvale, CA.`,
   duration: 'Oct 2020 - Apr 2021',
-  location: locations.belgrade,
-  companyHref: 'https://www.microsoft.com/en-rs/mdcs',
   companyLogo: 'microsoft.png',
   companyLogoStyle: { height: '30px' },
 });
