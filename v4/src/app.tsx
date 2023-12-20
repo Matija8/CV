1d499cb78f489f8e8e56ed723eb10bf1b5934bd7
matija8 <matijanme@gmail.com>
2023-12-20 22:48:38 +0100

    feat: scaffold vite cv

---
+++ v4/src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './app.scss';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <p>Hello world</p>
+  </React.StrictMode>,
+);
